83b0b04f5b50987e6c217c21df0d2f315182a557
Kacper Krawczyk <kacper.krawczyk@host.com>
2026-02-23 15:39:14 +0100

    1

---
+++ int/CITATION.R
@@ -0,0 +1,9 @@
+citHeader("Żeby cytować tę pracę::")
+
+citEntry(entry = "Manual",
+         title        = "IDEChoiceR: Pakiet R wspomagający wybór zintegrowanych środowisk programistycznych (IDE)",
+         author       = person("Kacper", "Krawczyk"),
+         year         = 2026,
+         note         = "Pakiet R, wersja 1.0",
+         textVersion  = "Kacper Krawczyk (2026))."
+)
